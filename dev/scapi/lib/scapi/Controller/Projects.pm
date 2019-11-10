6a70e92a4498841c37deea3720b9d2deee20a9a7
Jirawat Uttayaya <admin@apps.jirawat.com>
2019-11-10 15:31:03 -0500

    Projects Catalyst Controller

---
+++ dev/scapi/lib/scapi/Controller/Projects.pm
@@ -0,0 +1,48 @@
+package scapi::Controller::Projects;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+scapi::Controller::Projects - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->stash(projects => [$c->model('DB::Project')->all]);
+    $c->stash(template => 'projects/list.tt2');
+}
+
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Jirawat Uttayaya
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
